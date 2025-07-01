13793c4cbb039fca10f61e2859887605d2fafa63
hashem-mdoukh <hashmmdwkh@gmail.com>
2025-07-01 16:15:41 +0300

    Add New Section In App

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
 'use client'
-import axios from "axios";
+
+/** Components */
 import Navbar from "./components/Navbar";
+import Container from "./components/Container";
+import WeatherIcon from "./components/WeatherIcon";
+import WeatherDetails  from './components/WeatherDetails'
+
+/** Utils */
+import { getDayOrNightIcon } from "./utils/getDayOrNightIcon";
+import { convertKilvenToCelsius } from "./utils/convertKilvenToCelsius";
+import { metersToKilometers } from './utils/metersToKilometers'
+import { convertWindSpeed } from "./utils/convertWindSpeed";
+
+/** Fetch Wheather API  */
+import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import { format, parseISO } from "date-fns";
-import Container from "./components/Container";
-import { convertKilvenToCelsius } from "./utils/convertKilvenToCelsius";
-import WeatherIcon from "./components/WeatherIcon";
-import { getDayOrNightIcon } from "./utils/getDayOrNightIcon";
+
+/** Interfcaces */
 export interface WeatherForecastResponse {
   cod: string;
   message: number;
@@ -81,7 +92,7 @@ export interface Coordinates {
 
 export default function Home() {
 
- const {
+  const {
     data,
     isLoading,
     isError,
@@ -112,14 +123,14 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
-      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9  w-full  pb-10 pt-4">
         <section>
           <div>
             <h2 className="flex gap-1 text-2xl items-end">
-              <p>{ format(parseISO(firstData?.dt_txt ?? ""), "EEEE") }</p>
-              <p className="text-lg">({ format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy") })</p>
+              <p>{format(parseISO(firstData?.dt_txt ?? ""), "EEEE")}</p>
+              <p className="text-lg">({format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")})</p>
             </h2>
-            <Container className=" gap-10 px-6 items-center">
+            <Container className=" gap-10 px-6 items-center mt-4">
               <div className="flex flex-col px-4">
                 <span className="text-5xl">
                   {convertKilvenToCelsius(firstData?.main.temp ?? 0)}°
@@ -133,10 +144,10 @@ export default function Home() {
                     {convertKilvenToCelsius(firstData?.main.temp_min ?? 0)}
                     °↓ {" "}
                   </span>
-                  <span> 
+                  <span>
                     {" "}
                     {convertKilvenToCelsius(firstData?.main.temp_max ?? 0)}
-                    °↑ 
+                    °↑
                   </span>
                 </p>
               </div>
@@ -144,12 +155,38 @@ export default function Home() {
                 {data?.list.map((d, i) =>
                   <div key={i} className="flex flex-col justify-between gap-2 items-center text-xs font-semibold">
                     <p className="whitespace-nowrap">{format(parseISO(d.dt_txt), 'h:mm a')}</p>
-                    <WeatherIcon iconName={d.weather[0].icon} />
                     <WeatherIcon iconName={getDayOrNightIcon(d.weather[0].icon, d.dt_txt)} />
                     <p>{convertKilvenToCelsius(d?.main.temp ?? 0)}°</p>
                   </div>
                 )}
               </div>
+            </Container>
+          </div>
+          <div className="flex gap-4 mt-4">
+            <Container className="w-fit  justify-center flex-col px-4 items-center ">
+              <p className="capitalize text-center">
+                {firstData?.weather[0].description}{" "}
+              </p>
+              <WeatherIcon
+                iconName={getDayOrNightIcon(
+                  firstData?.weather[0].icon ?? "",
+                  firstData?.dt_txt ?? ""
+                )}
+              />
+            </Container>
+
+            <Container className="bg-yellow-300/80  px-6 gap-4 justify-between overflow-x-auto">
+              <WeatherDetails
+                visability={metersToKilometers(
+                  firstData?.visibility ?? 10000
+                )}
+                airPressure={`${firstData?.main.pressure} hPa`}
+                humidity={`${firstData?.main.humidity}%`}
+                sunrise={format(data?.city.sunrise ?? 1702949452, "H:mm")}
+                // sunrise={}
+                sunset={format(data?.city.sunset ?? 1702517657, "H:mm")}
+                windSpeed={convertWindSpeed(firstData?.wind.speed ?? 1.64)}
+              />
             </Container>
           </div>
         </section>
